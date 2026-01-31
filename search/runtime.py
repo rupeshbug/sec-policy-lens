c8f762df983c854321257ae6dd2bdbb56e48d42f
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2026-01-31 15:01:20 +0545

    minor changes

--- search/runtime.py
+++ search/runtime.py
@@ -1,15 +1,10 @@
-# search/runtime.py
 from functools import lru_cache
 import os
+
 from groq import Groq
-from sentence_transformers import SentenceTransformer
-from transformers import AutoTokenizer, AutoModelForMaskedLM
 from qdrant_client import QdrantClient
 
 from search.query_decomposition import QueryDecomposer
-from sentence_transformers import  SentenceTransformer
-from qdrant_client import QdrantClient
-from ingest.reranker import CrossEncoderReranker
 
 
 @lru_cache
@@ -21,29 +16,6 @@ def get_decomposer():
 def get_llm_client():
     return Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-@lru_cache
-def get_dense_model():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    return SentenceTransformer(
-        "sentence-transformers/all-MiniLM-L6-v2",
-        device=device
-    )
-
-
-@lru_cache
-def get_splade():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    tokenizer = AutoTokenizer.from_pretrained(
-        "naver/splade-cocondenser-ensembledistil"
-    )
-    model = AutoModelForMaskedLM.from_pretrained(
-        "naver/splade-cocondenser-ensembledistil"
-    ).to(device)
-
-    model.eval()
-    return tokenizer, model, device
-
 
 @lru_cache
 def get_qdrant():
@@ -51,13 +23,3 @@ def get_qdrant():
         url=os.getenv("QDRANT_URL"),
         api_key=os.getenv("QDRANT_API_KEY"),
     )
-
-
-@lru_cache
-def get_embedder():
-    return SentenceTransformer("all-MiniLM-L6-v2")
-
-
-@lru_cache
-def get_cross_encoder_reranker():
-    return CrossEncoderReranker()
